b49e7e3308cbf393b4c2a22723696dca943e98f1
AKEJU FATAI <=>
2022-10-23 05:37:57 +0100

    [update] added role-acess checker

--- src/handlers/game-api.handler.ts
+++ src/handlers/game-api.handler.ts
@@ -3,8 +3,9 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 import { GameValidator } from "../validators";
 import { HandleExceptions } from "../decorators";
-import { ResponseHandler } from "../helpers";
+import { IAuthTokenPayload } from "../interfaces";
 import { GameService } from "../services/game.service";
+import { ResponseHandler, RoleAccess, verifyAuthToken } from "../helpers";
 
 const GAME_SERVICE = Container.get(GameService);
 
@@ -17,6 +18,9 @@ export class GameApiHandler {
    */
   @HandleExceptions()
   async createGame(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    const AUTH_DATA: IAuthTokenPayload = verifyAuthToken(event.headers);
+    RoleAccess.checkThatUserIsAuthorized(AUTH_DATA.isAdmin);
+
     const REQ_BODY = JSON.parse(event.body as string);
     await GameValidator.checkCreateGame(REQ_BODY);
 
@@ -31,6 +35,9 @@ export class GameApiHandler {
    */
   @HandleExceptions()
   async updateGame(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+    const AUTH_DATA: IAuthTokenPayload = verifyAuthToken(event.headers);
+    RoleAccess.checkThatUserIsAuthorized(AUTH_DATA.isAdmin);
+
     const GAME_ID: string = event.pathParameters?.gameId as string; // WHAT IF THIS IS `undefined`
     const REQ_BODY = JSON.parse(event.body as string);
 
---
+++ src/helpers/role-access.ts
@@ -0,0 +1,21 @@
+import { UnauthorizedError } from "../exceptions";
+
+/**
+ * @class RoleAccess
+ */
+export class RoleAccess {
+
+    /**
+     * @method checkThatUserIsAuthorized
+     * @static
+     * @param {boolean} isAdmin 
+     */
+    static checkThatUserIsAuthorized(isAdmin: boolean): void {
+      if(isAdmin) {
+          return;
+      }
+
+      throw new UnauthorizedError("Unauthorized access!");
+    }
+
+}
