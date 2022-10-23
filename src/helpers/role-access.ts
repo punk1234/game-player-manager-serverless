a8d017994e75ef52403ac3ebe02cd9de1977152c
AKEJU FATAI <=>
2022-10-23 05:39:10 +0100

    fix lint

--- src/helpers/role-access.ts
+++ src/helpers/role-access.ts
@@ -4,18 +4,16 @@ import { UnauthorizedError } from "../exceptions";
  * @class RoleAccess
  */
 export class RoleAccess {
-
-    /**
-     * @method checkThatUserIsAuthorized
-     * @static
-     * @param {boolean} isAdmin 
-     */
-    static checkThatUserIsAuthorized(isAdmin: boolean): void {
-      if(isAdmin) {
-          return;
-      }
-
-      throw new UnauthorizedError("Unauthorized access!");
+  /**
+   * @method checkThatUserIsAuthorized
+   * @static
+   * @param {boolean} isAdmin
+   */
+  static checkThatUserIsAuthorized(isAdmin: boolean): void {
+    if (isAdmin) {
+      return;
     }
 
+    throw new UnauthorizedError("Unauthorized access!");
+  }
 }
